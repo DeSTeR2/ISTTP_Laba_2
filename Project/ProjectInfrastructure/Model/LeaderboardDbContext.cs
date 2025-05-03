c72db4c837dcc6cab53a297ad5d1449b93ff2f21
RbDg <defens213@gmail.com>
2025-05-03 20:14:07 +0300

    add .net project with db setuo

---
+++ Project/ProjectInfrastructure/Model/LeaderboardDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectInfrastructure.Model;
+
+public partial class LeaderboardDbContext : DbContext
+{
+    public LeaderboardDbContext()
+    {
+    }
+
+    public LeaderboardDbContext(DbContextOptions<LeaderboardDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+        optionsBuilder.UseSqlServer("Server=Admin\\SQLEXPRESS; Database=LeaderboardDB; Trusted_Connection=True; TrustServerCertificate=True; ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
